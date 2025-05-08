8d7188030219dc8ac4df5d74afa7b058b14c99e6
joseamd <jose.alexander.munoz@correounivalle.edu.co>
2025-05-08 12:02:02 -0500

    cambios deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../backend/frontend/dist',  // Ubicación donde se generarán los archivos
+    outDir: '../frontend/dist',  // Ubicación donde se generarán los archivos
   },
 })
