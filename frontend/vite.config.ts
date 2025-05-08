6a4b91a3586824c7f48cf04eaa32dae00b233120
joseamd <jose.alexander.munoz@correounivalle.edu.co>
2025-05-08 14:56:42 -0500

    crear super usuario Django

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/static/',  // 👈 esto le dice a Vite que use rutas relativas al STATIC_URL de Django
   build: {
     outDir: '../frontend/dist',  // El destino de build
     emptyOutDir: true,
