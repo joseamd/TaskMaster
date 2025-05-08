5ab45212bf4a808e866fdf3a60b9b6a40824946f
joseamd <jose.alexander.munoz@correounivalle.edu.co>
2025-05-08 00:27:55 -0500

    Configuración de producción para React y Django

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Login from './components/Login/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,10 +9,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Redirige / a /login */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
